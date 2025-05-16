98d10229f02ec0773bb46ad362c424ce36e232c3
Trần Phương Huyền <tranphuonghuyen2005@gmail.com>
2025-05-16 20:20:27 +0700

    add medium and hard exercise

--- hard.py
+++ hard.py
@@ -10,7 +10,7 @@ def findNumberSubarray(arr, k):
   return num  
 
 
-#6 (NOT YET)find largest product sub array
+#6find largest product sub array
 def largestProductSub(arr): 
   arr.sort()
   res = arr[0]
@@ -20,3 +20,37 @@ def largestProductSub(arr):
       currentPro -= arr[j]
       res = max(res,currentPro)
     return res
+  
+#21 number of trailling zero in factorial 
+def factorial(num): 
+  if num == 0: 
+    return 1
+  else: 
+    return num * factorial(num-1)
+  
+def trailingZeroFactorial(num): 
+  fact = factorial(num)
+  count = 0 
+  while (fact % 10 == 0): 
+    count += 1
+    fact = fact / 10
+  return count
+
+#28 find the nth prime number 
+def thenNthPrime(n): 
+  if n < 1: 
+    return -1
+  count = 0 
+  num = 1
+  while (count < n): 
+    num += 1
+    for i in range(2, num): 
+      if (num % i == 0): 
+        break
+    else: 
+      count += 1
+  return num
+
+#65 merge interval 
+
+#66 number of ways to express n as the sum of two or more consecutive positive integers
